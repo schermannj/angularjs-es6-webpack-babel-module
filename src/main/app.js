c7b194cc8404b60fe57135619d7e08a445ce5816
V.Sheruda <v.sheruda@aimprosoft.com>
2016-10-28 15:55:56 +0300

    added vendor module

--- src/main/app.js
+++ src/main/app.js
@@ -1,22 +1,5 @@
 'use strict';
 
-// angular and other global libraries
-import angular from 'angular';
-import uirouter from 'angular-ui-router';
-import translate from 'angular-translate';
-import toaster from 'angularjs-toaster';
-import ngAnimate from 'angular-animate';
-import uibootstrap from 'angular-ui-bootstrap';
-import smartTable from 'angular-smart-table';
-import 'angular-busy';
-import 'bootstrap/dist/js/bootstrap.min.js';
-
-// styles
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/css/bootstrap-theme.min.css';
-import 'angularjs-toaster/toaster.scss';
-import 'angular-busy/angular-busy.css';
-
 // local configuration files
 import routeConfig from './configs/route.config';
 import langConfig from './configs/lang.config';
@@ -31,29 +14,15 @@ import HomePageController from './controllers/home-page/home-page.controller';
 import userService from './services/user.service';
 import httpRequestInterceptor from './services/http-request.interceptor';
 
-// components and directives
-import homePageComponent from './components/home-page/home-page.component';
-import helloDirective from './components/hello/hello.directive';
-
 // run functions
 import {onStateChangeError} from './configs/state-events.handler';
 import registerAngularBusy from './components/angular-busy/angular-busy.run';
 
-// filters
-import upperCaseFilterFunction from './common/filters/upper-case.filter';
+// vendor
+import vendorModule from './vendor';
 
 const moduleName = angular
-    .module('app', [
-        uirouter,
-        translate,
-        toaster,
-        ngAnimate,
-        uibootstrap,
-        smartTable,
-        'cgBusy',
-        homePageComponent,
-        helloDirective
-    ])
+    .module('app', [vendorModule])
     .config(routeConfig)
     .config(langConfig)
     .config(stateConfig)
@@ -62,7 +31,6 @@ const moduleName = angular
     .controller('HomePageController', HomePageController)
     .service('userService', userService)
     .factory('httpRequestInterceptor', httpRequestInterceptor)
-    .filter('upperCase', upperCaseFilterFunction)
     .run(onStateChangeError)
     .run(registerAngularBusy)
     .name;
